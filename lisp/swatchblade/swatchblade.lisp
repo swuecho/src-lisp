09e3c70c6bed8684b8e99df7b62094215efc4cec
Hao Wu <echowuhao@gmail.com>
2016-12-28 12:35:49 -0800

    Update swatchblade.lisp

--- lisp/swatchblade/swatchblade.lisp
+++ lisp/swatchblade/swatchblade.lisp
@@ -5,3 +5,8 @@
 ;;; "swatchblade" goes here. Hacks and glory await!
 
 (defun square_echo (x) (* x x))
+
+;; With the current package set to the COM.GIGAMONKEYS.EMAIL-DB package,
+;; other than names inherited from the COMMON-LISP package,
+;; you can use any name you want for whatever purpose you want.
+(defun square (x) (* x x))
